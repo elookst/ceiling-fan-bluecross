5eda78ae8ac834ee5f0420e0b1b49ee799772339
Erica <eluksts@gmail.com>
2022-07-29 11:23:37 -0400

    testing code included and display properties included. comments added for methods, classes, etc.

--- CeilingFan.java
+++ CeilingFan.java
@@ -4,59 +4,86 @@
  * PURPOSE      :   This class models the attributes and methods of a ceiling fan. The class contains
  *                  two PullCord objects, which can alter the speed or direction that the fan may be rotating.
  *                  The fan possesses three different speeds or can be turned off completely.
- * ASSUMPTIONS  :   The fan starts in OFF mode and counterclockwise default direction.
+ * ASSUMPTIONS  :   The fan starts in OFF mode and forward default direction.
  */
 
 public class CeilingFan {
     
     // constants
-    final int FANOFF = 0; // fan speed settings
+    // the constants here are used to set fan speed settings and fan directions
+    final int FANOFF = 0; 
     final int FANSPEEDLOW = 1;
     final int FANSPEEDMED = 2;
     final int FANSPEEDHIGH = 3;
 
-    final boolean CLOCKWISE = true; // fan direction, might change these to forward / reverse
-    final boolean COUNTERCLOCKWISE = false;
+    final boolean FORWARD = true; 
+    final boolean REVERSE = false;
 
 
     // attributes
     private int rotationSpeed; 
     private boolean rotationDirection; 
 
+    // returns fan speed
     public int getSpeed()
     {
         return rotationSpeed;
     }
+    // sets the fan speed
     public void setSpeed(int speed)
     {
         this.rotationSpeed = speed;
     }
 
+    // returns fan direction
     public boolean getDirection()
     {
         return rotationDirection;
     }
+    // sets the fan direction
     public void setDirection(boolean direction)
     {
         this.rotationDirection = direction;
     }
 
+    // Abstract Class PullCord
+    // This class sets the requirements for each pull cord
+    // It specifies that each pull cord must be able to be pulled by the user
+    // A future version of this class could implement a cord length or label attribute
     abstract class PullCord {
 
-        // maybe attribute here for cord length
+        // maybe abstract int attribute here for cord length if it is determined that each
+        // pull cord has a different length for differing between each other
+        // alternatively, maybe there is a label or tag on each pullcord (string)
 
         abstract void Pull();
     }  
 
+
+    // CLASS    :   DirectionPullCord
+    // This class is a type of pull cord, that contains one method for pulling the cord
+    // Pulling the cord will reverse the current direction that the fan is rotating
     class DirectionPullCord extends PullCord {
 
+        // attribute for pull cord length or possibly a label on the cord (string)
+        
+        // reverses the direction of the ceiling fan
         public void Pull() {
             setDirection(!(getDirection()));
         }
     }
 
+
+    // CLASS    :   SpeedPullCord
+    // This class is a type of pull cord, that contains one method for pulling the cord
+    // Pulling the cord will adjust the speed of the fan
     class SpeedPullCord extends PullCord {
 
+        // attribute for pull cord length or possible a label on the cord (string)
+
+
+        // Increases the speed of the fan unless current speed is MAX
+        // If at max speed, it will turn off the fan.
         public void Pull() {
             if (getSpeed() != FANSPEEDHIGH)
             {
@@ -70,22 +97,65 @@ public class CeilingFan {
     }
 
 
-
-    // constructor
+    // constructor - creates the ceiling fan object and sets initial values for parameters
     // note: assumed default values
     public CeilingFan() {
         rotationSpeed = FANOFF;
-        rotationDirection = COUNTERCLOCKWISE;
+        rotationDirection = FORWARD;
     }
 
-
+    // Main method
+    // Creates a new ceiling fan object and pull cord objects
+    // returns void
     public static void main(String[] args) {
         CeilingFan fan = new CeilingFan();
 
         CeilingFan.DirectionPullCord directionCord = fan.new DirectionPullCord();
         CeilingFan.SpeedPullCord speedCord = fan.new SpeedPullCord();
 
+        // The code below used for testing purposes only.
+
+        fan.DisplayProperties(); // direction fwd, speed 0
+        
+        directionCord.Pull();
+        fan.DisplayProperties(); // direction rev, speed 0
+
+        directionCord.Pull();
+        fan.DisplayProperties(); // direction fwd, speed 0
+
+        speedCord.Pull();
+        fan.DisplayProperties(); // direction fwd, speed 1
+        
+        speedCord.Pull();
+        fan.DisplayProperties(); // direction fwd, speed 2
+        
+        speedCord.Pull();
+        directionCord.Pull(); 
+        fan.DisplayProperties(); // direction rev, speed 3
+        
+        speedCord.Pull();
+        fan.DisplayProperties(); // direction rev, speed 0
+        speedCord.Pull();
+        fan.DisplayProperties(); // direction rev, speed 1
+
         
     }
+
+    public void DisplayProperties() {
+        System.out.print("Direction:");
+
+        if (this.getDirection())
+        {
+            System.out.println("FORWARD");
+        }
+        else
+        {
+            System.out.println("REVERSE");
+        }
+        System.out.print("Speed:");
+        System.out.println(this.getSpeed());
+        System.out.println("===========");
+    }
+
 
 }
